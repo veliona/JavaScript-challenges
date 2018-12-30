33bd7c4b5f0eccafc314d4b6e3f4b33227f95945
veliona <jakubczak.urszula.julia@gmail.com>
2018-12-30 14:40:17 +0100

    Create Temperature

---
+++ Temperature.js
@@ -0,0 +1,21 @@
+const n = parseInt(readline());
+var inputs = readline().split(" ");
+var numTemp = [];
+var curr = inputs[0];
+var diff = Math.abs(0 - curr);
+
+if (n === 0 || inputs === undefined) {
+  print("0");
+} else {
+  for (var val = 0; val < inputs.length; val++) {
+    numTemp.push(Number(inputs[val]));
+    var newdiff = Math.abs(0 - inputs[val]);
+    if (newdiff < diff) {
+      diff = newdiff;
+      curr = inputs[val];
+    } else if (newdiff == diff && curr < 0) {
+      curr = inputs[val];
+    }
+  }
+  print(curr);
+}
